1346e38300cee247cea84a90c0213ee09189a0e0
erikwahlstrom <54457926+erikwasa@users.noreply.github.com>
2020-08-19 16:01:59 +0200

    first

---
+++ LogFileReader/Observation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogFileReader
+{
+    public class Observation
+    {
+        public string UserName { get; set; }
+        public bool Success { get; set; }
+        public DateTime Time { get; set; }
+        public string IpAddress { get; set; }
+        public string SessionId { get; set; }
+    }
+}
---
+++ LogFileReader/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogFileReader
+{
+    public class Session
+    {
+        public Session()
+        {
+            PartialObservations = new List<Observation>();
+        }
+        public List<Observation> PartialObservations { get; set; }
+        public string IpAddress { get; set; }
+        public string SessionId { get; set; }
+    }
+}
